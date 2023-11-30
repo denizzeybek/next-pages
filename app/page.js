54c50bd26d1d18e410cf545c0eb346096b70d242
denizzeybek <zeybekdeniz64@gmail.com>
2023-11-30 12:55:46 +0300

    try new google analytics package

--- app/page.js
+++ app/page.js
@@ -1,14 +1,3 @@
 export default function Home() {
-  if (process.env.NEXT_PUBLIC_TEST === 'dev') {
-    alert('dev')
-  } else {
-    alert('prod')
-  }
-  return (
-    <>
-      {/* <GoogleAnalytics trackPageViews /> */}
-
-      <div>ana ekran {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} </div>
-    </>
-  );
-  }
+  return <div>ana ekran </div>;
+}
